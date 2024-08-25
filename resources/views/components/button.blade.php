349a662dd70dd63f0077a2328cdc7aedeb54b465
Dym Sohin <you@example.com>
2024-08-25 14:01:52 +0200

    tailwind

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+@props( ['type'] )
+
+<a {{ $attributes->merge(['class'=>'rounded-md bg-gray w-10 h-10 py-2 px-2 border-2 content-center justify-center leading-4 flex']) }}
+    ><div class=""
+        >{{ $slot }}</div></a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>inventory</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body class="px-5 py-5 min-h-screen flex">
 
-<x-button>
+  <nav class="flex flex-col min-h-full space-y-4">
+    <x-button href="/">/</x-button>
+    <x-button href="/archive" title="archive">A</x-button>
+    <div class="grow"></div>
+    <x-button href="/settings" title="settings">S</x-button>
+  </nav>
 
-</x-button>
+  <main class="ml-5">
+    xxx
+  </main>
+
+
 
 </body>
 </html>
